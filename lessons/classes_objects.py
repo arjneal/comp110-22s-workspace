2a1c074c7bdd73e4c56951ffb421b3082a093890
Neal Kapur <neal.kapur@unc.edu>
2022-03-29 12:33:33 -0400

    update

--- lessons/classes_objects.py
+++ lessons/classes_objects.py
@@ -8,10 +8,17 @@ class Pizza:
     """Models the idea of a Pizza."""
  
     # Attribute Definitions
-    size: str
-    toppings: int
-    extra_cheese: bool
-    def price(self) -> float:
+    size: str = "small"
+    toppings: int = 0
+    extra_cheese: bool = False 
+    
+    def __init__(self, size: str, toppings: int, extra_cheese: bool):
+        """Constructor definition for initialization of attributes."""
+        self.size = size
+        self.toppings = toppings 
+        self.extra_cheese = extra_cheese
+    
+    def price(self, tax: float) -> float:
         """Calculate the price of a pizza."""
         total: float = 0.0
         if self.size == "large":
@@ -27,12 +34,12 @@ class Pizza:
         return total 
 
 
-a_pizza: Pizza = Pizza()
-a_pizza.size = "large"
-a_pizza.toppings = 3
-a_pizza.extra_cheese = False
+a_pizza: Pizza = Pizza("large", 3)
 print(Pizza)
 print(a_pizza)
 # When we print a_pizza its an object. 
 print(a_pizza.size)
-print(f"Price ${a_pizza.price()}")
+print(f"Price ${a_pizza.price(1.05)}")
+
+another_pizza: Pizza = Pizza("small", 0)
+print(a_pizza.size)
